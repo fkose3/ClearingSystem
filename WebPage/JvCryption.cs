febee63c7aed7b23528b45145684f231cf557fd7
fkose3 <fkose3@gmail.com>
2014-01-22 22:10:43 +0200

    Kullanıcı şifreleme
    Kullanıcı hesapları şifleme sistemi

--- WebPage/JvCryption.cs
+++ WebPage/JvCryption.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace WebPage
@@ -27,6 +29,30 @@ namespace WebPage
                 return_value = Convert.ToChar(current - i++).ToString();
             }
             return return_value;
+        }
+
+        public string GetUserAccountPassword(string value)
+        {
+            return GetCryptionText(MD5Hash(value)).Substring(0,16).ToUpper();
+        }
+
+        public string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits
+                //for each byte
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
         }
     }
 }
